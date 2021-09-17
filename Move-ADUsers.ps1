1a0581695575fcc948428f5803c81e4546401f52
Sev2050 <84805721+Sev2050@users.noreply.github.com>
2021-09-17 15:06:17 -0400

    Create Move-ADUsers.ps1

---
+++ Move-ADUsers.ps1
@@ -0,0 +1,42 @@
+$OU1 = "AdminSV"
+$OU2 = "EVPA"
+
+
+$OUInfo1 = Get-ADOrganizationalUnit "OU=Users,OU=AdminSV,DC=campusad,DC=msu,DC=edu"
+$OUInfo2 = Get-ADOrganizationalUnit "OU=Resources,OU=EVPA,DC=campusad,DC=msu,DC=edu"
+$ADUsers = Get-ADUser -SearchBase $OUInfo1 -Filter * -Properties ProxyAddresses,mail,Department,displayName
+$Department = "Office of the Executive Vice President for Administration"
+
+Foreach ($ADUser in $ADUsers)
+{
+
+
+    #Setting variable for correcting the Common Name so that the Distinguished Name will reflect changes
+    $CorrectedName = $ADUser.Name.Replace($OU1,$OU2)
+
+
+    #Setting to variable for removing old OU name from UserPrincipalName
+    $CorrectedSamAccountName = $ADUser.SamAccountName.Replace($OU1,$OU2)
+
+    #Setting to variable for removing old OU name from UserPrincipalName
+    $CorrectedDisplayName = $ADUser.DisplayName.Replace($OU1,$OU2)
+
+    #Set new email address
+    $CorrectEmailAddress = $ADUser.mail.Replace($OU1,$Ou2)
+   
+    #Rename the AD User
+    Set-ADUser -Identity $ADUser -DisplayName $CorrectedDisplayName -SamAccountName $CorrectedSamAccountName -EmailAddress $CorrectEmailAddress -add @{ProxyAddresses="smtp:$CorrectEmailAddress"} -Department $Department
+
+    #Removing from the account for the Common Name
+    $NewName = $ADUser | Rename-ADObject -NewName $CorrectedName -PassThru
+
+    #Note of accounts being modified
+    $OutputCorrectedAccounts += $NewName.Name
+
+    Start-Sleep -Second 1
+
+    #Move AD User Object
+    Move-ADObject -Identity $NewName -TargetPath $OUInfo2
+
+}
+
